e8f186cf793138b3fe7b65549fab81078a6dafae
mohammadrezaie82 <mohammadrezaie1382422@gmail.com>
2025-01-19 09:05:28 +0330

    initial commit

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,26 @@
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
+import MenuOverview from './MenuOverview';
+
+function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <>
+      <ul className="divide-y divide-stone-200 px-2">
+        {menu.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.id} />
+        ))}
+      </ul>
+      <MenuOverview />
+    </>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
---
+++ src/features/menu/MenuOverview.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getTotalCartPrice, getTotalCartQuantity } from '../cart/cartSlice';
+import { formatCurrency } from '../../utils/helpers';
+
+function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
+      </p>
+      <Link to="/cart">Open cart &rarr;</Link>
+    </div>
+  );
+}
+
+export default CartOverview;
